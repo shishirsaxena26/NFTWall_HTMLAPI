06ec6030460ce4894257ea6b4289a1f889a2d636
Shishir Saxena <shishir.sna99@gmail.com>
2026-03-12 22:00:16 +0530

    Auto update Thu 03/12/2026 22:00:16.04

--- node_api/server.js
+++ node_api/server.js
@@ -1,15 +1,28 @@
+const fs = require('fs');
 const sql = require('mssql');
+const sql2 = require('mssql');
 const express = require('express');
 const app = express();
-const NodeCache = require('node-cache');
-const cache = new NodeCache({ stdTTL: 120 }); // 1 minute cache
-let { bech32, bech32m } = require('bech32');
+const https = require('https');
+const cors = require('cors');
 
+app.use(cors({
+  origin: ['https://nftwall.io', 'http://localhost:5173', 'http://localhost:8010',  'http://ezetrade.ai', 'https://eze.stbots.io'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+app.use(express.json());
+
+const sslOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
 
 const config = {
         user: 'rptuser',
         password: '*8WK*G?*uBweN$5_=U$MY',
-        server: '52.220.77.185',
+        server: 'EC2AMAZ-J2PGR5M',
         database: 'reportdb',
         synchronize: true,
         trustServerCertificate: true,
@@ -26,7 +39,28 @@ const config = {
         }
 }
 
+const Dconfig = {
+        user: 'dUser',
+        password: 'D@User!786',
+        server: 'EC2AMAZ-J2PGR5M',
+        database: 'DAccount',
+        synchronize: true,
+        trustServerCertificate: true,
+        pool: {
+            max: 10,
+            min: 0,
+            idleTimeoutMillis: 30000
+        },
+        options: {
+            trustedConnection: true,
+            encrypt: true,
+            enableArithAbort: true,
+            trustServerCertificate: true,
+        }
+}
+
 let pool; // will hold our connection pool
+let poolDAccount; // will hold our connection pool
 
 async function getPool() {
   try {
@@ -44,21 +78,64 @@ async function getPool() {
   }
 }
 
+async function getPoolDAccount() {
+  try {
+    if (!poolDAccount || !poolDAccount.connected) {
+      console.log('🔄 (Re)connecting sql2 pool...');
+      poolDAccount = await new sql2.ConnectionPool(Dconfig).connect();
+      console.log('✅ sql2 Connected');
+    }
+	return poolDAccount;
+  } catch (err) {
+    console.error('❌ sql2 connect failed:', err.message);
+    // wait a bit before retrying
+    await new Promise((r) => setTimeout(r, 2000));
+    return getPoolDAccount(); // recursive retry
+  }
+}
+
+
+async function callProcedureDAccount(procedureName, params = {}) {
+    try {
+		console.log("callProcedureDAccount");
+     // const pool = await sql.connect(config);
+      // ✅ get the shared pool (auto-reconnect if broken)
+      const poolDAccount = await getPoolDAccount();
+      
+      // ✅ create a request from the pool
+      const request = poolDAccount.request();
+  
+      // Add input parameters dynamically
+      for (const [key, value] of Object.entries(params)) {
+        request.input(key, value);
+      }
+  
+      const result = await request.execute('sp_'+procedureName);
+      return result.recordsets; // Return only recordset or full result //result.recordset || result; 
+    } catch (err) {
+      console.error('SQL error:', err);
+      throw err;
+    }
+  }
+// Example Express.js API endpoint
+
 async function callProcedure(procedureName, params = {}) {
     try {
+		console.log("callProcedure");
      // const pool = await sql.connect(config);
       // ✅ get the shared pool (auto-reconnect if broken)
       const pool = await getPool();
       
       // ✅ create a request from the pool
       const request = pool.request();
+  
       // Add input parameters dynamically
       for (const [key, value] of Object.entries(params)) {
         request.input(key, value);
       }
-
+  
       const result = await request.execute('sp_'+procedureName);
-      return result.recordset || result; // Return only recordset or full result
+      return result.recordsets; // Return only recordset or full result //result.recordset || result; 
     } catch (err) {
       console.error('SQL error:', err);
       throw err;
@@ -67,60 +144,56 @@ async function callProcedure(procedureName, params = {}) {
 // Example Express.js API endpoint
 
 
-app.get('/api/call-proc', async (req, res) => {
+app.get('/api/dacct-proc', async (req, res) => {
     const procedureName = req.query.procedureName;
     
     // Remove procedureName from the query params
     const { procedureName: _, ...params } = req.query;
-
-    if (params.address) {
-      let cacheKey = `${params.address}`;
-      const cached = cache.get(cacheKey);
-      if (cached) return res.json(cached);
-    }
-    
+  
     if (!procedureName) {
       return res.status(400).json({ success: false, error: 'procedureName is required' });
     }
 
 
     try {
-        const result = await callProcedure(procedureName, params);
+        
+		const result = await callProcedureDAccount(procedureName, params);
         res.json({ success: true, data: result });
+		
       } catch (err) {
         res.status(500).json({ success: false, error: err.message });
     }
 
 });
 
-app.listen(8090, () => {
-  console.log('Server running on http://localhost:3000');
+app.get('/api/call-proc', async (req, res) => {
+    const procedureName = req.query.procedureName;
+    
+    // Remove procedureName from the query params
+    const { procedureName: _, ...params } = req.query;
   
-// example validator operator bech32
-const valoperAddr = "ethm1lm4nqcac7uk0hnmfhtsy2y0agutaw0appul49t";
-
-console.log("valoperAddr:", valoperAddr);
-// decode bech32
-const { words } = bech32.decode(valoperAddr);
-const bytes = bech32.fromWords(words);
-
-// convert to hex (0x...)
-const hex = "0x" + Buffer.from(bytes).toString("hex");
-
-console.log("Hex address:", hex);
+    if (!procedureName) {
+      return res.status(400).json({ success: false, error: 'procedureName is required' });
+    }
 
 
-
-const mymetamaskvalidator="0xFEeB3063B8f72CfBCF69Bae04511fD4717D73fA1";
-// Remove 0x and convert to bytes
-const bytes1 = Buffer.from(mymetamaskvalidator.replace(/^0x/, ""), "hex");
-
-// Encode to Bech32 with validator prefix
-const valoperAddrnew = bech32.encode("ethmvaloper", bech32.toWords(bytes1));
-
-console.log("Ether Validator Operator Address:", valoperAddrnew);
+    try {
+        
+		const result = await callProcedure(procedureName, params);
+        res.json({ success: true, data: result });
+		
+      } catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
 
 });
+var port = process.env.PORT || 2096;
+
+
+https.createServer(sslOptions, app).listen(2096, () => {
+  console.log('✅ HTTPS server running at https://localhost:8443');
+});
+
 
 
 //npm install -g pm2
@@ -128,7 +201,7 @@ console.log("Ether Validator Operator Address:", valoperAddrnew);
 
 //pm2 logs nodeapi    # see logs
 
-//pm2 start server.js --name nodeapi
+//pm2 start server.js --name nodeapi -i 3
 //pm2 restart server.js --name nodeapi -i 3
 
 //pm2 list
@@ -136,4 +209,3 @@ console.log("Ether Validator Operator Address:", valoperAddrnew);
 
 //pm2 stop all
 //pm2 delete all
-
