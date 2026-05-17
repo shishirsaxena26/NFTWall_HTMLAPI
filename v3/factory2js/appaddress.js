c93e7d652c3d7285e708bcf97d837aa9d917fe77
Shishir Saxena <shishir.sna99@gmail.com>
2026-05-17 14:58:34 +0530

    Auto update Sun 05/17/2026 14:58:34.63

--- v3/factory2js/appaddress.js
+++ v3/factory2js/appaddress.js
@@ -3,7 +3,7 @@ const providerMain = "https://chain.ozonescan.com";
 
 const ZERO = "0x0000000000000000000000000000000000000000";
 //let hexBaseAddress = "0x519445d757C82c8576F400F1a55d65f3e2635169";
-let hexBaseAddress = "0xDe62eF6A5c5509D01d4f258e0E325F0861fF5B8E";
+let hexBaseAddress = "0xcE70138c1de6B2901F2A6011f1c499B1d95bAf84";
 const minBlock = 3884916;
 
 const DEAD = "0x000000000000000000000000000000000000dEaD"
