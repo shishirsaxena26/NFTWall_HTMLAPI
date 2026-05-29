2ab7bcb5a21317fe1b5832e9f854c6c5a69cceef
Shishir Saxena <shishir.sna99@gmail.com>
2026-05-29 23:38:07 +0530

    Auto update Fri 05/29/2026 23:38:07.19

--- v3/factory2js/app.js
+++ v3/factory2js/app.js
@@ -2761,10 +2761,10 @@ async function onTVLRefresh() {
 
         // Enable wallet
         const nestedContractV1 = new web3T.eth.Contract(INested741ABI.abi, inNested741);
-        const Nested741TVL = new web3T.eth.Contract([{ "type": "function", "name": "TVLrefresh", "inputs": [{ "name": "user", "type": "address", "internalType": "address" }], "outputs": [], "stateMutability": "nonpayable" }], '0x7f4331Bb1f776fb0F21b996432e5D8380D2e8efb');
+        const Nested741TVL = new web3T.eth.Contract([{ "type": "function", "name": "JoinAsUser", "inputs": [], "outputs": [], "stateMutability": "nonpayable" }, { "type": "function", "name": "TVLrefresh", "inputs": [{ "name": "user", "type": "address", "internalType": "address" }, { "name": "maxint", "type": "uint256", "internalType": "uint256" }], "outputs": [], "stateMutability": "nonpayable" }], '0x97d0C37812089F58C8b4cf3ce753d81E9f6e44c1');
 
         const tx = await Nested741TVL.methods
-            .TVLrefresh(user)
+            .TVLrefresh(user, 2)
             .send({
                 from: accounts[0]
             });
