f36687bb82a168e02acf4ce1b724a82866c2a952
Shishir Saxena <shishir.sna99@gmail.com>
2026-06-14 05:05:20 +0530

    Auto update Sun 06/14/2026  5:05:20.21

--- v3/factory2js/app.js
+++ v3/factory2js/app.js
@@ -45,7 +45,7 @@ let currentAccount = null;
 let currentInstance = null;
 let currentStor = null;
 
-const maxintervals = 16;
+const maxintervals = 8;
 const doTVLClaim = false;
 const doClaim = false;
 
